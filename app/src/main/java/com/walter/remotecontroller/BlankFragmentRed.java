5f628d0df87221df460b3e77908cb9757e71a661
walter <walter52@sohu.com>
2024-08-19 00:33:21 +0800

    动态添加fragment

---
+++ app/src/main/java/com/walter/remotecontroller/BlankFragmentRed.java
@@ -0,0 +1,27 @@
+package com.walter.remotecontroller;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * create an instance of this fragment.
+ */
+public class BlankFragmentRed extends Fragment {
+
+
+    private View root;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        if (root == null) {
+            root = inflater.inflate(R.layout.fragment_blank_red, container, false);
+        }
+        return root;
+    }
+}
--- app/src/main/java/com/walter/remotecontroller/MainActivity.java
+++ app/src/main/java/com/walter/remotecontroller/MainActivity.java
@@ -1,14 +1,17 @@
 package com.walter.remotecontroller;
 
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.walter.remotecontroller.databinding.ActivityMainBinding;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private ActivityMainBinding binding;
 
@@ -21,8 +24,26 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        Button btn = findViewById(R.id.btn1);
-        btn.setOnClickListener(v -> Log.i(TAG, "on click"));
+        Button btn1 = findViewById(R.id.btn1);
+        btn1.setOnClickListener(this);
+        Button btn2 = findViewById(R.id.btn2);
+        btn2.setOnClickListener(this);
     }
 
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.btn1) {
+            replaceColour(new BlankFragmentYellow());
+        } else if (v.getId() == R.id.btn2) {
+            replaceColour(new BlankFragmentRed());
+        }
+
+    }
+
+    private void replaceColour(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frag_default, fragment);
+        fragmentTransaction.commit();
+    }
 }
